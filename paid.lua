3eb23dbd9c79c1b7eddb018296eb86408184f61f
yumehatdog <dezqpal@gmail.com>
2026-04-26 07:35:48 +0800

    Add Aiden67e3 to whitelisted users with 5m trial

--- paid.lua
+++ paid.lua
@@ -1,7 +1,7 @@
       -- [[ 1. CONFIGURATION ]]
 local WhitelistedUsers = {
     -- Pwede mo i-set gamit ang "m" (minutes), "h" (hours), "d" (days)
-    [""]   = "",   -- 1 Minute Trial
+    ["Aiden67e3"]   = "5m",   -- 1 Minute Trial
     ["ingella_199"] = "6h",  -- 24 Hours
     ["primobns21"]  = -1,     -- Lifetime Access
 }
