28a066c0ed667ec27f5c55c63c26ee69f68724af
yumehatdog <dezqpal@gmail.com>
2026-04-26 09:12:59 +0800

    Update paid.lua

--- paid.lua
+++ paid.lua
@@ -3,7 +3,7 @@ local WhitelistedUsers = {
     -- Siguraduhin na walang space sa dulo ng pangalan
     ["ingella_199"] = "10h",
     [""] = "2w",
-    ["Aiden67e3"] = "1m",
+    ["Aiden67e3"] = "30s",
     ["primobns21"] = -1,
 }
 
